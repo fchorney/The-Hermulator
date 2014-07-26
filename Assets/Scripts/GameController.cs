6d8c236c764fd53b503db4ee4e8f34434e2a2b5e
Josh Jaques <jjaques@gmail.com>
2014-07-25 23:54:19 -0500

    make level scroll

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,8 +3,15 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
+	public Transform Level;
+	private float LevelSpeed = 6f;
+
 	public void Start() {
 		// Turn off gravity for the game
 		Physics2D.gravity = Vector2.zero;
+	}
+
+	public void Update() {
+		Level.transform.position -= Vector3.up * LevelSpeed * Time.deltaTime;
 	}
 }

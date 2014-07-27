f4c51ef55a35fd68ed1672e9b8f998b8a58150cb
Fernando Chorney <noobsauce@djsbx.com>
2014-07-27 11:38:52 -0500

    Rewrote the music controller. Technically this could be used with multiple levels where each level has an intro/loop/boss tracks. Would need to be modified if it had more than 3 tracks. Now you can transition to the boss music at any point.

--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -3,88 +3,94 @@ using System.Collections;
 
 public class MusicController : MonoBehaviour {
 
-	public AudioClip boss;
-	public AudioClip intro;
-	public AudioClip loop;
-	public float introDelay;
-	public float loopDelay;
-	public float fadeDelay;
-	public float fadeSpeed;
+	public AudioClip boss = null;
+	public AudioClip intro = null;
+	public AudioClip loop = null;
+	public float introDelay = 0f;
+	public float loopDelay = 0f;
+	public float fadeDelay = 0f;
+	public float fadeSpeed = 0f;
 
 	public bool enableBoss = false;
 
-	private AudioSource audio_one = null;
-	private AudioSource audio_two = null;
+	private AudioSource audio_intro = null;
+	private AudioSource audio_loop = null;
+	private AudioSource audio_boss = null;
 
 	enum StageState { level = 0, boss = 1 };
-
 	private StageState stagestate;
 
+	// Volume Fade for the loop music going into the boss music
 	private float fade;
 
+	// Determine if the boss music has been triggered
 	private bool boss_started;
 
 	// Use this for initialization
 	void Start () {
+		// Initialize our Audio Sources
+		audio_intro = gameObject.AddComponent<AudioSource> ();
+		audio_loop = gameObject.AddComponent<AudioSource> ();
+		audio_boss = gameObject.AddComponent<AudioSource> ();
+	
+		// Initialize our audio clip properties
+		audio_intro.clip = intro;
+		audio_intro.loop = false;
+		audio_intro.Stop ();
+
+		audio_loop.clip = loop;
+		audio_loop.loop = true;
+		audio_loop.Stop ();
+
+		audio_boss.clip = boss;
+		audio_boss.loop = true;
+		audio_boss.Stop ();
+
+		// Initialize our private variables
+		fade = 1.0f; // Initial fade is 1.0f, aka: 100% volume
+		boss_started = false; //At the start of the level, obviously we havent started the boss
+		// Assume we dont start at a boss (because that's fucking stupid)
 		stagestate = StageState.level;
-
-		audio_one = gameObject.AddComponent<AudioSource> ();
-		audio_two = gameObject.AddComponent<AudioSource> ();
-
-		audio_one.clip = intro;
-		audio_one.loop = false;
-
-		audio_two.clip = loop;
-		fade = 1.0f;
-
-		boss_started = false;
-
-		audio_one.Stop ();
-		audio_two.Stop ();
-
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		switch (stagestate) {
 			case StageState.level:
-			// If neither intro nor loop are playing...
-					if (!audio_one.isPlaying && !audio_two.isPlaying) {
-							// Play intro and set up loop clip
-							audio_one.PlayDelayed (introDelay);
-							audio_two.loop = true;
-							audio_two.PlayDelayed (introDelay + loopDelay);
-					}
-
-			// If the loop is playing
-					if (!audio_one.isPlaying) {
-							// Load up the boss music in audio one
-							audio_one.clip = boss;
-							audio_one.loop = true;
-					}
-					break;
+				// Assuming nothing is playing yet, start the intro and loop
+				if (!audio_intro.isPlaying && !audio_loop.isPlaying) {
+						// Start playing the intro and main loop using the delay variables
+						audio_intro.PlayDelayed (introDelay);
+						audio_loop.PlayDelayed (loopDelay + introDelay);
+				}
+			break;
 			case StageState.boss:
-			// Loop music is still playing and boss music isn't
-					if (!boss_started) {
-							// Fade out loop and playdelay boss music?
-							
-							audio_one.PlayDelayed (fadeDelay);
-							boss_started = true;
-					}
+			// If the boss music hasn't started to play yet...
+				if (!boss_started) {
+						// Start playing the boss music with the fadeout delay
+						audio_boss.PlayDelayed (fadeDelay);
+						boss_started = true;
+				}
 
-					if (audio_two.isPlaying) {
-							if (fade <= 0.0f) {
-									audio_two.Stop ();
-							} else {
-									audio_two.volume = fade;
-									fade -= fadeSpeed;
-							}
-					}
-					break;
-			}
+			// If either the intro or the main loop is still playing...
+				if (audio_intro.isPlaying || audio_loop.isPlaying) {
+						// If the fade has gone all the way down to 0, then we can stop the intro/loop music
+						if (fade <= 0.0f) {
+								audio_intro.Stop ();
+								audio_loop.Stop ();
+						} else { // Intro/Loop are still fading out, continue to lower the volume for the fade
+								audio_intro.volume = fade;
+								audio_loop.volume = fade;
+								fade -= fadeSpeed;
+						}
+				}
+			break;
+		}
+
+		// If at any time the enableBoss boolean is set to true, transition to the boss state
 		if (enableBoss) {
 				stagestate = StageState.boss;
 		}
 	}
 }
+// Written by The Biggest

1d4c48ec72f6d87c5340f1e003ab1853467b7397
Karel Kahula <karel.kahula@telenium.ca>
2014-07-27 16:23:46 -0500

    improve spawner-mob movement.

--- Assets/Scripts/MovementPatterns/Spiral.cs
+++ Assets/Scripts/MovementPatterns/Spiral.cs
@@ -2,15 +2,13 @@
 using System.Collections;
 
 public class Spiral : EnemyMovementPattern {
-	float amplitude = 3f;
-	float frequency = 0.5f;
 	float circleSize = 3f;
 	float circleSpeed = 3;
 
 	public override void Move (GameObject obj) {
 		Vector3 position = obj.transform.position;
 		position.x -= Mathf.Sin((Time.time - activeTime) * circleSpeed) * circleSize * Time.deltaTime;
-		position.y -= Mathf.Cos((Time.time - activeTime) * circleSpeed) * circleSize * Time.deltaTime;
+		position.y -= Mathf.Abs(Mathf.Cos((Time.time - activeTime) * circleSpeed) * circleSize * Time.deltaTime);
 		obj.transform.position = position;
 	}
 }

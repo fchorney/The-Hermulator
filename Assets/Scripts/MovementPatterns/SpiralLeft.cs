960ecb0c8321972296088e16ad519a20dbff2faf
Karel Kahula <karel.kahula@telenium.ca>
2014-07-27 16:57:08 -0500

    spawner-mob movement right.

---
+++ Assets/Scripts/MovementPatterns/SpiralLeft.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiralLeft : EnemyMovementPattern {
+	float circleSize = 6;
+	float circleSpeed = 3;
+	protected enum Direction {
+		Left=0,
+		Right
+	}
+
+	public override void Move(GameObject obj) {
+		spiral(obj, Direction.Left);
+	}
+
+	protected void spiral (GameObject obj, Direction direction) {
+		Vector3 position = obj.transform.position;
+		if(direction == Direction.Left) {
+			position.x -= Mathf.Sin((Time.time - activeTime) * circleSpeed) * circleSize * Time.deltaTime;
+		}
+		else {
+			position.x += Mathf.Sin((Time.time - activeTime) * circleSpeed) * circleSize * Time.deltaTime;
+		}
+		position.y -= Mathf.Abs(Mathf.Cos((Time.time - activeTime) * circleSpeed) * circleSize * Time.deltaTime);
+		obj.transform.position = position;
+	}
+}
---
+++ Assets/Scripts/MovementPatterns/SpiralRight.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiralRight : SpiralLeft {
+
+	public override void Move(GameObject obj) {
+		spiral(obj, Direction.Right);
+	}
+}

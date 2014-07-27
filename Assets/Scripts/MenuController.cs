96ca4577a82769f211bdef91a7f48d98d466f7b7
Josh Jaques <jjaques@gmail.com>
2014-07-27 18:44:57 -0500

    get things working

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -4,19 +4,41 @@ using System.Collections;
 public class MenuController : MonoBehaviour {
 	private bool on_credits = false;
 
+	public BoxCollider2D StartB;
+	public BoxCollider2D Credits;
+
+	private Rect rStart;
+	private Rect rCredits;
+
+	Rect MakeRect(BoxCollider2D box) {
+		Bounds b = box.bounds;
+		
+		Vector2 min = Camera.main.WorldToScreenPoint(new Vector2(b.min.x, b.max.y));
+		Vector2 max = Camera.main.WorldToScreenPoint(new Vector2(b.max.x, b.min.y));
+		
+		Rect r = new Rect(min.x, Screen.height - min.y, max.x - min.x, Mathf.Abs(max.y - min.y));
+		
+		return r;
+	}
+
+	void Start() {
+		rStart = MakeRect(StartB);
+		rCredits = MakeRect(Credits);
+	}
+
 	void OnGUI() {
 		if (on_credits) {
-			if (GUI.Button (new Rect (640, 550, 80, 30), "Back")) {
+			if (Input.GetMouseButtonDown(0)) {
 				Vector3 pos = camera.transform.position;
 				pos.y = 0f;
 				camera.transform.position = pos;
 				on_credits = false;
 			}
 		} else {
-			if (GUI.Button (new Rect (450, 540, 100, 40), "Start")) {
-				Application.LoadLevel ("Level1");
+			if (GUI.Button (rStart, "Start")) {
+				Application.LoadLevel ("Level 1");
 			}
-			if (GUI.Button (new Rect (600, 540, 100, 40), "Credits")) {
+			if (GUI.Button (rCredits, "Credits")) {
 				Vector3 pos = camera.transform.position;
 				pos.y = -19.2f;
 				camera.transform.position = pos;
